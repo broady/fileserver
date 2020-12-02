283c553c3ef0428a619e88bf7e620ba36ac88d98
Chris Broadfoot <cbro@golang.org>
2020-12-02 03:09:22 -0800

    support 404.html as a catchall

--- main.go
+++ main.go
@@ -15,6 +15,18 @@ func main() {
 				"\tfileserver :9999 .\n\n")
 		os.Exit(2)
 	}
-	http.Handle("/", http.FileServer(http.Dir(os.Args[2])))
+	http.Handle("/", http.FileServer(custom404fs{http.Dir(os.Args[2])}))
 	log.Fatal(http.ListenAndServe(os.Args[1], nil))
+}
+
+type fileSystem struct {
+	base http.FileSystem
+}
+
+func (fs fileSystem) Open(name string) (http.File, error) {
+	f, err := fs.base.Open(name)
+	if os.IsNotExist(err) {
+		f, err = fs.base.Open("404.html")
+	}
+	return f, err
 }
